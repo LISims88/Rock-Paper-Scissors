33aaee241c47791d789380e31086e3b7c44d4be9
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-03-01 10:19:57 -0600

    Provide functionality to the classic game.

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 //Data Model
 var classic = ['rock', 'paper', 'scissors']
+var human = {
+    wins: 0,
+    selection: ''
+}
+var computer = {
+    wins: 0,
+    selection: ''
+}
 //DOM Variable
+//classic
+var gameWeapons = document.querySelector('.game-weapons')
 var rock = document.querySelector('.rock')
 var paper = document.querySelector('.paper')
 var scissors = document.querySelector('.scissors')
- var lizard = document.querySelector('lizard')
-var alien = document.querySelector('.alien')
-var differentGameButton = document.querySelector('.button')
-var classicGame = document.querySelector('.classic')
-var challengeGame = document.querySelector('.challenge')
-var classicBoard = document.querySelector('.classic-game-board')
-var challengeBoardAdd = document.querySelector('.challenge-game-add')
+
+var humanWins = document.querySelector('.human-wins')
+var computerWins = document.querySelector('.computer-wins')
 var weapons = document.querySelector('.weapons')
 var gameChoice = document.querySelector('.game-choice')
+var classicGame = document.querySelector('.classic')
+var challengeGame = document.querySelector('.challenge')
+var differentGameButton = document.querySelector('.button')
+
+var classicBoard = document.querySelector('.classic-game-board')
+
+//challenge
+var lizard = document.querySelector('lizard')
+var alien = document.querySelector('.alien')
+var challengeBoardAdd = document.querySelector('.challenge-game-add')
+
 //Eventlisteners
 classicGame.addEventListener('click', showClassicGame)
 challengeGame.addEventListener('click', showChallengeGame)
@@ -20,6 +37,17 @@ differentGameButton.addEventListener('click', function(){
     returnHome()
     challengeBoardAdd.classList.add('hidden')
 } )
+gameWeapons.addEventListener('click', function(event){
+    if(event.target.className === 'rock') {
+        console.log(event)
+        human.selection = 'rock'
+    } else if(event.target.className === 'paper'){
+        human.selection = 'paper'
+    } else if(event.target.className === 'scissors'){
+        human.selection = 'scissors'
+    }
+    createComputer()
+})
 //Event Handlers
 function showClassicGame(){
     classicGame.classList.toggle('hidden')
@@ -45,7 +73,20 @@ function returnHome(){
         !showChallengeGame()
     } 
 }
-
+function rockWins(){
+    paper.classList.add('hidden')
+}
+function paperWins(){
+    scissors.classList.add('hidden')
+}
+function scissorsWins(){
+    rock.classList.add('hidden')
+}
+function resetGameBoard(){
+    rock.classList.remove('hidden')
+    scissors.classList.remove('hidden')
+    paper.classList.remove('hidden')
+}
 
 //Functions
 function getRandomIndex(array){
@@ -53,10 +94,53 @@ function getRandomIndex(array){
     return array[randomIndex]
 }
 
-function humanPlay () {
-    
+function createComputer(){
+    computer.selection = getRandomIndex(classic)
+    getResults()
 }
 
-function createComputer(){
-    computerPlay = getRandomIndex(classic)
+function getResults(){
+    if((human.selection === 'rock' && computer.selection === 'scissors') || (computer.selection === 'rock' && human.selection === 'scissors')){
+        rockWins()
+        weapons.innerText = `Rock beats Scissors`
+    } else if ((human.selection === 'paper' && computer.selection === 'rock') || (computer.selection === 'paper' && human.selection === 'rock')){
+        paperWins()
+        weapons.innerText = `Paper beats Rock`
+    }else if ((human.selection === 'scissors' && computer.selection === 'paper') || (computer.selection === 'scissors' && human.selection === 'paper')){
+        scissorsWins()
+        weapons.innerText = `Scissors beats Paper`
+    } else{
+        weapons.innerText = `The game is a Draw`
+    }
+    getWins();
+    setTimeout(function(){
+        resetGameBoard()}, 8000)
+    
 }
+function getWins(){
+      if ((human.selection === 'rock' && computer.selection === 'scissors') || (human.selection === 'paper' && computer.selection === 'rock') || (human.selection === 'scissors' && computer.selection === 'paper')){
+        humanWins.innerText = human.wins += 1
+    } else if((computer.selection === 'rock' && human.selection === 'scissors') || (computer.selection === 'paper' && human.selection === 'rock') || (computer.selection === 'scissors' && human.selection === 'paper')){
+        computerWins.innerText = computer.wins += 1
+    } else{
+        return
+    }
+}
+
+
+
+
+
+// code needed 
+//(human.selection === 'rock' && computer.selection === 'lizard') || (computer.selection === 'rock' && human.selection === 'lizard')
+
+//(human.selection === 'paper' && computer.selection === 'alien') || (computer.selection === 'paper' && human.selection === 'alien')
+
+//(human.selection === 'scissors' && computer.selection === 'lizard') || (computer.selection === 'scissors' && human.selection === 'lizard')
+
+//(human.selection === 'lizard' && computer.selection === 'paper')|| (computer.selection === 'lizard' && human.selection === 'paper') ||(human.selection === 'lizard' && computer.selection === 'alien') || (computer.selection === 'lizard' && human.selection === 'alien')
+
+
+////(human.selection === 'alien' && computer.selection === 'scissors')|| (computer.selection === 'alien' && human.selection === 'scissors') ||(human.selection === 'alien' && computer.selection === 'rock') || (computer.selection === 'alien' && human.selection === 'rock')
+
+
